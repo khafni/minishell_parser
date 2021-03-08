b7078b1032916fdcdd7e1dd5fd4fcf5d64a02c94
Khalil Hafni <khafni@e2r2p6.1337.ma>
2021-03-08 16:59:04 +0100

    env variables lacks only the ? support

---
+++ libft/rstr_methods1.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rstr_methods1.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khafni <khafni@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/17 12:12:05 by khafni            #+#    #+#             */
+/*   Updated: 2021/03/06 16:18:46 by khafni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*rstr_to_cstr(t_rstr rs)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = malloc(rs->len + 1);
+	while (i < rs->len)
+	{
+		str[i] = rs->data[i];
+		i++;
+	}
+	str[rs->len] = '\0';
+	return (str);
+}
+
+char    rstr_get(t_rstr rs, int index)
+{
+        if (index < 0 || index >= rs->len)
+                return (-42);
+        return (*(rs->data + index));
+}
