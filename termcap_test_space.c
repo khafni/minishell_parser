e5b730a41402c7ef9b4559cdf3ac80769c0d7e14
Khalil Hafni <khafni@e1r1p8.1337.ma>
2021-05-16 19:59:15 +0100

    ok

--- termcap_test_space.c
+++ termcap_test_space.c
@@ -6,7 +6,16 @@
 
 int main(int argc, char *argv[], char **envp)
 {
-    printf("%s", getenv("TERM"));
-    tgetent(NULL, getenv("TERM"));
+    char *term_type = getenv("TERM");
+    /* printf("%s", getenv("TERM"));
+    tgetent(NULL, getenv("TERM")); */
+    int ret = tgetent(NULL, term_type);
+    if (ret != 1)
+    {
+        printf("can't access termcaps data base the data base");
+        return (1);
+    }
+    printf("%d\n", tgetnum("co"));
+    printf("%d\n", tgetflag("os"));
     return (0);
 }
